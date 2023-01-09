aa2c43f4cea251cbc615f213acd905af6d888a83
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2023-01-09 13:25:57 +0400

    check

--- index.js
+++ index.js
@@ -13,57 +13,57 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // mongodb+srv://avrsifdbk:<password>@cluster0.aozwcec.mongodb.net/?retryWrites=true&w=majority
-DB_PASS=y1qyhEah5wfmrPLq
-DB_ADMIN=avrsifdbk
-DB_HOST=cluster0.aozwcec.mongodb.net/fdbdb
-const mongoUri = `mongodb+srv://${DB_ADMIN}:${DB_PASS}@${DB_HOST}?retryWrites=true&w=majority`
+// DB_PASS=y1qyhEah5wfmrPLq
+// DB_ADMIN=avrsifdbk
+// DB_HOST=cluster0.aozwcec.mongodb.net/fdbdb
+// const mongoUri = `mongodb+srv://${DB_ADMIN}:${DB_PASS}@${DB_HOST}?retryWrites=true&w=majority`
 
-mongoose.connect(mongoUri,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+// mongoose.connect(mongoUri,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// })
 const port = process.env.PORT || 3001
 app.listen(port,()=>{
     console.log('server is running on port',port)
 })
-app.post('/fdbck', (req, res) => {
-    const bdd = req.body
-    const fdb = new Fdbk
-    fdb.anonim=bdd.anonim
-    fdb.age=bdd.age
-    fdb.brand=bdd.brand
-    fdb.drugstore=bdd.drugstore
-    fdb.contact=bdd.contact
-    fdb.message=bdd.message
-    fdb.save()
+// app.post('/fdbck', (req, res) => {
+//     const bdd = req.body
+//     const fdb = new Fdbk
+//     fdb.anonim=bdd.anonim
+//     fdb.age=bdd.age
+//     fdb.brand=bdd.brand
+//     fdb.drugstore=bdd.drugstore
+//     fdb.contact=bdd.contact
+//     fdb.message=bdd.message
+//     fdb.save()
 
-    res.send('Ok');
-});
+//     res.send('Ok');
+// });
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
 });
-const frrr = mongoose.Schema({
-    anonim:{
-        type:String,
-        trim:true
-    },
-    age:{},
-    brand:{},
-    drugstore:{},
-    message:{},
-    contact:{},
-    extra:{}
-})
-app.get('/all',(req,res)=>{
-    const getAll = async()=>{
-        const Feedbacks  = mongoose.model('fdbks',frrr)
-        try{
-            const query = await Feedbacks.find({})
-            console.log(query)
-            res.send(query)
-        }catch(error){
-            throw error
-        }
-    }
-     getAll()
-})
+// const frrr = mongoose.Schema({
+//     anonim:{
+//         type:String,
+//         trim:true
+//     },
+//     age:{},
+//     brand:{},
+//     drugstore:{},
+//     message:{},
+//     contact:{},
+//     extra:{}
+// })
+// app.get('/all',(req,res)=>{
+//     const getAll = async()=>{
+//         const Feedbacks  = mongoose.model('fdbks',frrr)
+//         try{
+//             const query = await Feedbacks.find({})
+//             console.log(query)
+//             res.send(query)
+//         }catch(error){
+//             throw error
+//         }
+//     }
+//      getAll()
+// })
