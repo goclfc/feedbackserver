7abcdbf82f4d48f30eb5737443192ef50dd491a9
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2023-01-09 13:32:18 +0400

    Update index.js

--- index.js
+++ index.js
@@ -28,43 +28,43 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log("server is running on port", port);
 });
-// app.post('/fdbck', (req, res) => {
-//     const bdd = req.body;
-//     const fdb = new Fdbk;
-//     fdb.anonim=bdd.anonim;
-//     fdb.age=bdd.age;
-//     fdb.brand=bdd.brand;
-//     fdb.drugstore=bdd.drugstore;
-//     fdb.contact=bdd.contact;
-//     fdb.message=bdd.message;
-//     fdb.save();
-//     res.send('Ok');
-// });
+app.post('/fdbck', (req, res) => {
+    const bdd = req.body;
+    const fdb = new Fdbk;
+    fdb.anonim=bdd.anonim;
+    fdb.age=bdd.age;
+    fdb.brand=bdd.brand;
+    fdb.drugstore=bdd.drugstore;
+    fdb.contact=bdd.contact;
+    fdb.message=bdd.message;
+    fdb.save();
+    res.send('Ok');
+});
 app.get("/", (req, res) => {
   res.send("Hello World, from express");
 });
-const frrr = mongoose.Schema({
-  anonim: {
-    type: String,
-    trim: true,
-  },
-  age: {},
-  brand: {},
-  drugstore: {},
-  message: {},
-  contact: {},
-  extra: {},
-});
-app.get("/all", (req, res) => {
-  const getAll = async () => {
-    const Feedbacks = mongoose.model("fdbks", frrr);
-    try {
-      const query = await Feedbacks.find({});
-      console.log(query);
-      res.send(query);
-    } catch (error) {
-      throw error;
-    }
-  };
-  getAll();
-});
+// const frrr = mongoose.Schema({
+//   anonim: {
+//     type: String,
+//     trim: true,
+//   },
+//   age: {},
+//   brand: {},
+//   drugstore: {},
+//   message: {},
+//   contact: {},
+//   extra: {},
+// });
+// app.get("/all", (req, res) => {
+//   const getAll = async () => {
+//     const Feedbacks = mongoose.model("fdbks", frrr);
+//     try {
+//       const query = await Feedbacks.find({});
+//       console.log(query);
+//       res.send(query);
+//     } catch (error) {
+//       throw error;
+//     }
+//   };
+//   getAll();
+// });
